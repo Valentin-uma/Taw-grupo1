402f6d090d20c93503e0129a4a62b38d68a4e887
Valentin-uma <162116701+Valentin-uma@users.noreply.github.com>
2024-06-19 13:31:12 +0200

    Delete src directory

--- src/main/java/es/taw/grupo1/dao/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package es.taw.grupo1.dao;
-
-import es.taw.grupo1.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
--- src/main/java/es/taw/grupo1/dto/DTO.java
+++
@@ -1,5 +0,0 @@
-package es.taw.grupo1.dto;
-
-public interface DTO<DTOClass> {
-    public DTOClass toDTO ();
-}
